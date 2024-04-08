e380317297ee4503be95f73024d4ca6a1d58dd63
leon90dm <leon90dm@gmail.com>
2024-04-08 20:44:31 +0800

    main run api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import g4f
 import g4f.api
 
-print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
-g4f.api.run_api()
+
+if __name__ == "__main__":
+    print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
+    g4f.api.run_api()
